7fc83067ead675a43505ed762bc54263cd57e834
timo.engler <timo.engler@web.de>
2024-05-29 16:09:55 +0200

    movie-details start

--- movie-project/js/index.js
+++ movie-project/js/index.js
@@ -1,57 +1,100 @@
-const movieDbBaseUrl = 'https://api.themoviedb.org/3/';
+const global = {
+  currentPage: window.location.pathname,
+};
+
 const imageBaseUrl = 'https://image.tmdb.org/t/p/w500/';
 
-const apiKey =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmY2M4MDhhYjkxMzRmZjFjZDExYWIzNzNiYzliYTMwNyIsInN1YiI6IjY2NTA4MjdkODJhMmZmYWRmMDliMzgzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vt6eLn7mOhDWHxW3bY0MsL1yUPSjjnqzMYCGDRdj1h8';
+function init() {
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':
+      createSwiper();
+      displayNowPlayingMovies();
+      displayPopularMovies();
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      displayMovieDetails();
+      console.log('Movie details');
+      break;
+    case '/tv-details.html':
+      console.log('TV details');
+      break;
+    case '/search.html':
+      console.log('Search');
+      break;
+  }
 
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 4,
-  //   autoplay: {
-  //     delay: 2500,
-  //     disableOnInteraction: false,
-  //   },
+  highlightActiveLink();
+}
 
-  spaceBetween: 30,
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+init();
 
-  // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
-
-  // // Navigation arrows
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-
-async function getNowPlayingMovies() {
-  const response = await fetch(
-    movieDbBaseUrl +
-      `discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3`,
-    {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
+function highlightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
     }
-  );
+  });
+}
+
+async function fetchAPIData(endpoint) {
+  const API_KEY =
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmY2M4MDhhYjkxMzRmZjFjZDExYWIzNzNiYzliYTMwNyIsInN1YiI6IjY2NTA4MjdkODJhMmZmYWRmMDliMzgzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vt6eLn7mOhDWHxW3bY0MsL1yUPSjjnqzMYCGDRdj1h8';
+
+  const BASE_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(BASE_URL + endpoint, {
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  });
 
   const data = await response.json();
 
   console.log(data);
 
+  return data;
+}
+
+function createSwiper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: true,
+    },
+
+    spaceBetween: 30,
+    direction: 'horizontal',
+    loop: true,
+    freeMode: true,
+    breakpoints: {
+      // when window width is >= 320px
+      768: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
+
+async function displayNowPlayingMovies() {
+  const data = await fetchAPIData(
+    `discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3`
+  );
+
   inserSwipperSlides(data.results);
 }
 
-getNowPlayingMovies();
+async function displayPopularMovies() {
+  const data = await fetchAPIData(`movie/popular`);
+  console.log(data);
+
+  insertCards(data.results);
+}
 
 function createSwipperSlide(data) {
   const swipperSlide = document.createElement('div');
@@ -61,7 +104,7 @@ function createSwipperSlide(data) {
 
   swipperSlide.classList.add('swiper-slide');
 
-  swipperLink.href = data.href = '';
+  swipperLink.href = `/movie-details.html?id=${data.id}`;
   swipperImg.src = `${imageBaseUrl}${data.poster_path}`;
 
   swipperLink.appendChild(swipperImg);
@@ -69,15 +112,58 @@ function createSwipperSlide(data) {
   rating.classList.add('swiper-rating');
   rating.innerHTML = `<i class="fas fa-star text-secondary"></i> ${data.vote_average}/10`;
 
-  swipperSlide.appendChild(swipperImg);
+  swipperSlide.appendChild(swipperLink);
   swipperSlide.appendChild(rating);
 
   return swipperSlide;
 }
 
-async function inserSwipperSlides(elements) {
+function inserSwipperSlides(elements) {
   const swipper = document.querySelector('.swiper-wrapper');
   elements.forEach((element) => {
     swipper.appendChild(createSwipperSlide(element));
   });
+}
+
+function insertCards(elements) {
+  const popularMovies = document.querySelector('.popular-movies');
+
+  elements.forEach((element) => {
+    popularMovies.appendChild(createCard(element));
+  });
+}
+
+function createCard(data) {
+  const card = document.createElement('div');
+  const cardLink = document.createElement('a');
+  const cardImage = document.createElement('img');
+  const cardBody = document.createElement('div');
+
+  card.classList.add('card');
+
+  cardLink.href = `/movie-details.html?id=${data.id}`;
+  cardImage.src = `${imageBaseUrl}${data.poster_path}`;
+
+  cardBody.classList.add('card-body');
+
+  cardBody.innerHTML = `<p class="title">${data.original_title}</p><p>Release: ${data.release_date}</p>`;
+
+  cardLink.appendChild(cardImage);
+
+  card.appendChild(cardLink);
+  card.appendChild(cardBody);
+
+  return card;
+}
+
+async function displayMovieDetails() {
+  const id = getQueryParams('id');
+  console.log(id);
+}
+
+function getQueryParams(param) {
+  const url = new URL(window.location.href);
+  const params = new URLSearchParams(url.search);
+
+  return params.get(param);
 }
